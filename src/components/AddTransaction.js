adca54044baa14cce245c086a1b9a14aa1880c8c
Aladdin Sonni <aladdin.sonni@gmail.com>
2020-02-25 16:58:27 +0200

    Add react-hook-form

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,48 +1,77 @@
-import React, { useState, useContext } from 'react';
-import { GlobalContext } from '../context/GlobalState';
+import React from 'react';
+import clsx from 'clsx';
+import * as yup from 'yup';
+// import React, { useState, useContext } from 'react';
+import { useForm } from 'react-hook-form';
+
+// import { GlobalContext } from '../context/GlobalState';
+
+const schema = yup.object().shape({
+  text: yup.string().required(),
+  amount: yup.string().required()
+});
 
 export const AddTransaction = () => {
-  const [text, setText] = useState('');
-  const [amount, setAmount] = useState(0);
+  // const [text, setText] = useState('');
+  // const [amount, setAmount] = useState(0);
+  const { register, handleSubmit, errors } = useForm({
+    mode: 'onChange',
+    validationSchema: schema
+  });
 
-  const { addTransaction } = useContext(GlobalContext);
+  // const { addTransaction } = useContext(GlobalContext);
 
-  const onSubmit = e => {
-    e.preventDefault();
-    const newTransaction = {
-      id: Math.floor(Math.random() * 100000000),
-      text,
-      amount: +amount
-    };
-    addTransaction(newTransaction);
-    setText('');
-    setAmount(0);
+  const onSubmit = data => {
+    console.log(data);
+    // const newTransaction = {
+    //   id: Math.floor(Math.random() * 100000000),
+    //   text,
+    //   amount: +amount
+    // };
+    // addTransaction(newTransaction);
+    // setText('');
+    // setAmount(0);
   };
+
+  console.log(errors);
 
   return (
     <>
       <h3>Add new transaction</h3>
-      <form onSubmit={onSubmit}>
-        <div className="form-control">
+      <form onSubmit={handleSubmit(onSubmit)} noValidate>
+        <div
+          className={clsx({
+            'form-control': true,
+            error: errors.text
+            // success: !errors.text && !errors.text.message
+          })}
+        >
           <label htmlFor="text">Text</label>
           <input
+            name="text"
+            ref={register}
             type="text"
-            value={text}
-            onChange={e => setText(e.target.value)}
             placeholder="Enter text..."
           />
+          {errors.text && <small>{errors.text.message}</small>}
         </div>
-        <div className="form-control">
+        <div
+          className={clsx({
+            'form-control': true,
+            error: errors.amount
+            // success: errors && !errors.text
+          })}
+        >
           <label htmlFor="amount">
-            Amount <br />
-            (negative - expense, positive - income)
+            Amount <i>(negative - expense, positive - income)</i>
           </label>
           <input
+            name="amount"
+            ref={register}
             type="number"
-            value={amount}
-            onChange={e => setAmount(e.target.value)}
             placeholder="Enter amount..."
           />
+          {errors.amount && <small>{errors.amount.message}</small>}
         </div>
         <button className="btn">Add transaction</button>
       </form>
